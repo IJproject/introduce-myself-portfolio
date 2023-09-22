af26947554a562b539c42b279d790727cbc5a16a
IJproject <junchan3340@gmail.com>
2023-09-22 21:02:35 +0900

    微調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-const isButtonDisplay = ref(false)
-
-const displayButton = () => {
-    isButtonDisplay.value = true;
-}
-
 onMounted(() => {
     const leader = document.getElementById('leader')
     const follower = document.getElementById('follower')
@@ -15,14 +9,17 @@ onMounted(() => {
 
 <template>
     <div class="h-screen w-screen">
-        <div class="absolute top-0 left-0 h-screen w-screen flex justify-center items-center">
+        <div class="h-screen w-screen flex justify-center items-center">
+            <p>このページは作成中...</p>
             <p class="px-8 py-4 text-4xl text-white font-bold bg-transparent">
                 <span id="leader">W</span>
                 <span id="follower">elcome</span>
             </p>
+            <!-- このdivタグは消す -->
+            <div>
+                <NuxtLink to="/home" class="block px-3 py-2 bg-white text-black font-semibold">HOMEページはこちらから</NuxtLink>
+            </div>
         </div>
-        <div v-if="isButtonDisplay" class="absolute top-0 left-0 h-screen w-screen flex justify-center items-center">
-            <NuxtLink to="/home" class="block px-8 py-4 text-4xl text-white font-bold bg-transparent border-y-2 border-white">Enter</NuxtLink>
-        </div>
+        
     </div>
 </template>
